66fa5ca3a2477200d581e0b5e7223a877bf306e7
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2024-01-20 20:31:47 +0530

    Comment Addition

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-## GitShowCase
+# GitShowCase
+
+GitShowCase is a simple web application that allows users to showcase GitHub profiles and their public repositories. By entering a GitHub username, you can retrieve comprehensive information about the user, including a list of their public repositories.
+
+## How to Use GitShowCase
+
+1. Visit the GitShowCase web app.
+2. Enter the desired GitHub username in the provided input field and press Enter.
+
+## Features
+
+- **User Information:** Obtain details about the GitHub user, such as their name, bio, and avatar.
+- **Public Repositories:** View a list of public repositories associated with the provided username.
+- **Forked Repositories Filter:** GitShowCase filters out forked repositories, displaying only non-forked repositories.
+
+## Note
+
+Please note that GitShowCase focuses on showcasing non-forked repositories to highlight the original projects and contributions of the GitHub user. Also the search bar in list repositories section is only for ui purpose and is not functional
+
+Also note that some repos that does not contain topics and description, it is because these values are empty or null from the api received data.
+
+---
+
+# Hosted Here
+ [GitShowcase](https://alfiyasiddique.github.io/GitShowCase/).*
--- index.js
+++ index.js
@@ -1,48 +1,27 @@
-const rootRoute = "https://api.github.com/users/";
-let totalReposOfUser = 0;
-let numberOfRepoPerPage = document.getElementById("reponumber").value;
-let activePageId = "page1";
+const rootRoute = "https://api.github.com/users/";  // Github Api Root Route
+let totalReposOfUser = 0;                          //  Later contains the repo objects
+let numberOfRepoPerPage = document.getElementById("reponumber").value; // Perpage repo display value
+let activePageId = "page1";   // To change the active class of pagination number
 
-const getRequest = async (url) => {
-  let response = await fetch(url);
-  if (response.status === 200) {
-    response = await response.json();
-  } else {
-    response = false;
+// Function to add Pagination according to repos length
+const addPagination = (divide) => {
+  const start = `<li class="page-item"><a class="page-link" >Previ</a></li>`;
+  let pages = "";
+  let pagination = Math.ceil(totalReposOfUser.length / divide);
+  for (let i = 1; i <= pagination; i++) {
+    let active = i == 1 ? "active" : "";
+    pages += `<li class="page-item"><a id='page${i}' class="page-link ${active}">${i}</a></li>`;
   }
-  return response;
-};
-
-const populateProfile = (data) => {
-  const contentPart1 = `<div class="d-flex md:flex-wrap flex-row mb-3 ">
-  <div class="p-2"><img src="${data.img}" class='profile' alt="${data.name}-profile"></div>
-  <div class="d-flex flex-column mb-3">
-      <div class="p-2 username">${data.name}</div>
-      <div class="p-2">${data.bio}</div>`;
-
-  const contentPart2 = `<div class="p-2"><i class="fa fa-map-marker pe-3"></i>${data.location}</div>
-  <a class='link' target='_blank' href='https://twitter.com/${data.twitter}'class="p-2"><i class="fa fa-twitter pe-3"></i>https://twitter.com/${data.twitter}</a>`;
-
-  const footer = `</div>
-</div>
-<a target='_blank' href='${data.github}' class='link'><i class="fa fa-github pe-3"></i>${data.github}</a>
-`;
-  let html =
-    data.twitter && data.location
-      ? contentPart1 + contentPart2 + footer
-      : contentPart1 + footer;
-  document.getElementById("profile").innerHTML = html;
-};
-
-const returnTopicElement = (topic) => {
-  if (topic.length <= 0) return " ";
-  let template = ``;
-  for (let i = 0; i < topic.length; i++) {
-    template += `<li class='topic'>${topic[i]}</li>`;
+  const end = `<li class="page-item"><a class="page-link">Next</a></li>`;
+  let content = pages;
+  if (pagination > 10) {
+    content = start + content + end;
   }
-  return template;
+  document.getElementById("pagelist").innerHTML = content;
+  document.getElementById("page1").classList.add("active");
 };
 
+// Function to generate repo cards
 const generateTemplate = (repo1, repo2) => {
   const topic1 = returnTopicElement(repo1.topics);
   const topic2 = returnTopicElement(repo2.topics);
@@ -68,6 +47,66 @@ const generateTemplate = (repo1, repo2) => {
   return template;
 };
 
+// Function to make request to Github Api and return Response
+const getRequest = async (url) => {
+  let response = await fetch(url);
+  if (response.status === 200) {
+    response = await response.json();
+  } else {
+    alert("Enable to fetch data from server. Please try later.")
+    response = false;
+  }
+  return response;
+};
+
+// Function to implement pagination and change repositories on each page
+function pageClick() {
+  Array.from(document.getElementsByClassName("page-link")).forEach((page) => {
+    page.addEventListener("click", (e) => {
+      document.getElementById(activePageId).classList.remove("active");
+      page.classList.add("active");
+      activePageId = page.id;
+      let number = parseInt(page.innerText);
+      if (page.id == "page1") {
+        populateRepositories(
+          totalReposOfUser.slice(0, numberOfRepoPerPage)
+        );
+        return;
+      }
+      populateRepositories(
+        totalReposOfUser.slice(
+          (numberOfRepoPerPage* number) - numberOfRepoPerPage,
+          numberOfRepoPerPage* number
+        )
+      );
+    });
+  });
+}
+
+// Function to populate user account information
+const populateProfile = (data) => {
+  const contentPart1 = `<div class="d-flex md:flex-wrap flex-row mb-3 ">
+  <div class="p-2"><img src="${data.img}" class='profile' alt="${data.name}-profile"></div>
+  <div class="d-flex flex-column mb-3">
+      <div class="p-2 username">${data.name}</div>
+      <div class="p-2">${data.bio}</div>`;
+
+  const contentPart2 = `<div class="p-2"><i class="fa fa-map-marker pe-3"></i>${data.location}</div>
+  <a class='link' target='_blank' href='https://twitter.com/${data.twitter}'class="p-2"><i class="fa fa-twitter pe-3"></i>https://twitter.com/${data.twitter}</a>`;
+
+  const footer = `</div>
+</div>
+<a target='_blank' href='${data.github}' class='link'><i class="fa fa-github pe-3"></i>${data.github}</a>
+`;
+  let html =
+    data.twitter && data.location
+      ? contentPart1 + contentPart2 + footer
+      : contentPart1 + footer;
+  document.getElementById("profile").innerHTML = html;
+};
+
+
+// Function to populate all repositories of the user
 const populateRepositories = (data) => {
   let content = "";
   let i = 0;
@@ -109,46 +148,18 @@ const populateRepositories = (data) => {
   document.getElementById("repo-list").innerHTML = content+oddTemplate;
 };
 
-function pageClick() {
-  Array.from(document.getElementsByClassName("page-link")).forEach((page) => {
-    page.addEventListener("click", (e) => {
-      document.getElementById(activePageId).classList.remove("active");
-      page.classList.add("active");
-      activePageId = page.id;
-      let number = parseInt(page.innerText);
-      if (page.id == "page1") {
-        populateRepositories(
-          totalReposOfUser.slice(0, numberOfRepoPerPage)
-        );
-        return;
-      }
-      populateRepositories(
-        totalReposOfUser.slice(
-          (numberOfRepoPerPage* number) - numberOfRepoPerPage,
-          numberOfRepoPerPage* number
-        )
-      );
-    });
-  });
-}
 
-const addPagination = (divide) => {
-  const start = `<li class="page-item"><a class="page-link" >Previ</a></li>`;
-  let pages = "";
-  let pagination = Math.ceil(totalReposOfUser.length / divide);
-  for (let i = 1; i <= pagination; i++) {
-    let active = i == 1 ? "active" : "";
-    pages += `<li class="page-item"><a id='page${i}' class="page-link ${active}">${i}</a></li>`;
+// Function to get topic element values in list form
+const returnTopicElement = (topic) => {
+  if (topic.length <= 0) return " ";
+  let template = ``;
+  for (let i = 0; i < topic.length; i++) {
+    template += `<li class='topic'>${topic[i]}</li>`;
   }
-  const end = `<li class="page-item"><a class="page-link">Next</a></li>`;
-  let content = pages;
-  if (pagination > 10) {
-    content = start + content + end;
-  }
-  document.getElementById("pagelist").innerHTML = content;
-  document.getElementById("page1").classList.add("active");
+  return template;
 };
 
+// Event Handler run when a username is searched
 document.getElementById("search").addEventListener("keypress", async (e) => {
   if (e.key === "Enter") {
     const response = await getRequest(`${rootRoute}${e.target.value}`);
@@ -177,6 +188,7 @@ document.getElementById("search").addEventListener("keypress", async (e) => {
   }
 });
 
+// Event handler to make chnages in displayed data when repo per page value is changed
 document.getElementById("reponumber").addEventListener("change", (e) => {
   addPagination(e.target.value);
   pageClick();
